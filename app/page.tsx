d240d8f06682c7c90baf7b143f0696f159f53998
mayank-up <mayank.sonkar@upswing.one>
2026-01-05 16:21:01 +0530

    chore: redirection handler

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,53 @@
+"use client";
 
-import FlowOne from './flowOne';
+// export default function Home() {
+// return <div>
+//   <FlowOne/>
+// </div>
+// }
+
+import { useEffect, useState } from "react";
 
 export default function Home() {
-return <div>
-  <FlowOne/>
-</div>
+
+  const stage = 'https://redirection-handler.alpha.stage-upswing.one'
+  const local = 'http://localhost:8080'
+  const BASE = window.origin.includes('localhost')?local:stage
+  const redirectionHandler = `${BASE}/redirect/cct-auto-closure`;
+
+
+  const [countdown, setCountdown] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (countdown === null) return;
+
+    if (countdown === 0) {
+      window.location.href = redirectionHandler;
+      return;
+    }
+
+    const timer = setTimeout(() => {
+      setCountdown((prev) => prev ? prev - 1 : null);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [countdown]);
+
+  const handleClick = () => {
+    setCountdown(3);
+  };
+
+  return (
+    <div className="flex flex-col gap-5 justify-center items-center h-screen" style={{ padding: "20px" }}>
+    
+    
+    {countdown !== null && (
+        <p>Redirecting  in {countdown} seconds...</p>
+      )}
+      <button className="bg-black text-white p-4 rounded" onClick={handleClick} disabled={countdown !== null}>
+        Go to Page
+      </button>
+    </div>
+  );
 }
+
